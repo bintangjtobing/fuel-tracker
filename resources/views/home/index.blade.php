7083543b8b6d3fce22d48e1e2198dfe2f391a431
Bintang Tobing <47741235+bintangjtobing@users.noreply.github.com>
2024-04-05 01:50:51 +0700

    fix: table in index

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -80,104 +80,74 @@
     <div class="col-xxl-12 mb-25">
         <div class="card border-0">
             <div class="card-header">
-                <h6>Fuel & Services Track Activity</h6>
+                <h6 class="fs-14">Fuel & Services Track Activity</h6>
             </div>
             <div class="card-body p-0">
-                <table class="table mb-0">
-                    <thead>
-                        <tr class="userDatatable-header">
-                            <th>
-                                <div class="userDatatable-title">Fuel Type</div>
-                            </th>
-                            <th>
-                                <div class="userDatatable-title">Fuel Amount</div>
-                            </th>
-                            <th>
-                                <div class="userDatatable-title">Number of Liters</div>
-                            </th>
-                            <th>
-                                <div class="userDatatable-title">Service Date</div>
-                            </th>
-                            <th>
-                                <div class="userDatatable-title">Oil Name</div>
-                            </th>
-                            <th>
-                                <div class="userDatatable-title">Kilometers Traveled</div>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @forelse ($fuelEntriesData as $fuelEntry)
-                            <tr>
-                                <td>
-                                    <div class="d-flex">
-                                        <div class="userDatatable-inline-title">
-                                            <a href="#" class="text-dark fw-500">
-                                                <h6>{{ $fuelEntry->fuel_type ?? 'N/A' }}</h6>
-                                            </a>
-                                            <p class="pt-1 d-block mb-0">
-                                                Fuels Date: <b>{{ $fuelEntry->fuel_date ?? 'N/A' }}</b>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="d-flex">
-                                        <div class="userDatatable-inline-title">
-                                            <a href="#" class="text-dark fw-500">
-                                                <h6>Rp. {{ number_format($fuelEntry->fuel_amount, 2, ',', '.') ?? 'N/A' }}
-                                                </h6>
-                                            </a>
-                                            <p class="pt-1 d-block mb-0">
-                                                Fuels price /L: <b>Rp.
-                                                    {{ number_format($fuelEntry->fuel_price, 2, ',', '.') ?? 'N/A' }}</b>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="userDatatable-content">
-                                        @if ($fuelEntry->fuel_price != 0)
-                                            {{ number_format($fuelEntry->fuel_amount / $fuelEntry->fuel_price, 2, ',', '.') }}
-                                            Liters
-                                        @else
-                                            N/A
-                                        @endif
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="userDatatable-content">{{ $fuelEntry->service_date ?? 'N/A' }}</div>
-                                </td>
-                                <td>
-                                    <div class="d-flex">
-                                        <div class="userDatatable-inline-title">
-                                            <a href="#" class="text-dark fw-500">
-                                                <h6>{{ $fuelEntry->oil_name ?? 'N/A' }}</h6>
-                                            </a>
-                                            <p class="pt-1 d-block mb-0">
-                                                Oil Type: <b>{{ $fuelEntry->oil_type ?? 'N/A' }}</b>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </td>
-                                <td>
-                                    <div class="userDatatable-content">
-                                        {{ number_format($fuelEntry->kilometers_traveled, 2, ',', '.') ?? 'N/A' }} KM
-                                    </div>
-                                </td>
+                <div class="table-responsive">
+                    <table class="table table-sm mb-0">
+                        <thead>
+                            <tr class="userDatatable-header">
+                                <th>Fuel Type</th>
+                                <th>Fuel Amount (Rp)</th>
+                                <th>Number of Liters</th>
+                                <th>Service Date</th>
+                                <th>Oil Name</th>
+                                <th>Kilometers Traveled (KM)</th>
                             </tr>
-                        @empty
-                            <tr>
-                                <td colspan="8" class="text-center">
-                                    <div class="userDatatable-content">No data available in the database</div>
-                                </td>
-                            </tr>
-                        @endforelse
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @forelse ($fuelEntriesData as $fuelEntry)
+                                <tr>
+                                    <td>
+                                        <div class="fw-400 fs-13">{{ $fuelEntry->fuel_type ?? 'N/A' }}</div>
+                                        <div class="text-muted fs-12">Fuels Date:
+                                            <b>{{ $fuelEntry->fuel_date ?? 'N/A' }}</b></div>
+                                    </td>
+                                    <td>
+                                        <div class="fw-400 fs-13">Rp.
+                                            {{ number_format($fuelEntry->fuel_amount, 2, ',', '.') ?? 'N/A' }}</div>
+                                        <div class="text-muted fs-12">Fuels price /L: <b>Rp.
+                                                {{ number_format($fuelEntry->fuel_price, 2, ',', '.') ?? 'N/A' }}</b></div>
+                                    </td>
+                                    <td>
+                                        <div class="fw-400 fs-13">
+                                            @if ($fuelEntry->fuel_price != 0)
+                                                {{ number_format($fuelEntry->fuel_amount / $fuelEntry->fuel_price, 2, ',', '.') }}
+                                                Liters
+                                            @else
+                                                N/A
+                                            @endif
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="fw-400 fs-13">{{ $fuelEntry->service_date ?? 'N/A' }}</div>
+                                    </td>
+                                    <td>
+                                        <div class="fw-400 fs-13">{{ $fuelEntry->oil_name ?? 'N/A' }}</div>
+                                        <div class="text-muted fs-12">Oil Type: <b>{{ $fuelEntry->oil_type ?? 'N/A' }}</b>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="fw-400 fs-13">
+                                            {{ number_format($fuelEntry->kilometers_traveled, 2, ',', '.') ?? 'N/A' }} KM
+                                        </div>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr>
+                                    <td colspan="6" class="text-center">
+                                        <div class="text-muted fs-13">No data available in the database</div>
+                                    </td>
+                                </tr>
+                            @endforelse
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
+
+
 
 
 @endsection
